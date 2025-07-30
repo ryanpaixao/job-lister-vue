2d5ac13cbbbbfaddeaf74f633c16cc2f88d70d15
Ryan Paixao <16196485+ryanpaixao@users.noreply.github.com>
2025-07-30 15:48:19 -0300

    To App, Add: Navbar, Hero, HomeCards, JobListings

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,32 +1,13 @@
+<script setup>
+import Navbar from '@/components/Navbar.vue';
+import Hero from '@/components/Hero.vue';
+import HomeCards from '@/components/HomeCards.vue';
+import JobListings from './components/JobListings.vue';
+</script>
+
 <template>
-  <h1>Vue Jobs</h1>
+  <Navbar />
+  <Hero title="Test Title" subtitle="Test subtitle" />
+  <HomeCards />
+  <JobListings :limit="3" :showButton="true" />
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
