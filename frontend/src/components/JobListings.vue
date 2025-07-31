2ccf926af0ceaf79c8b91ff2d0c3707bf59dd5d2
Ryan Paixao <16196485+ryanpaixao@users.noreply.github.com>
2025-07-31 20:48:48 -0300

    Replace Anchor links w/RouterLinks

--- frontend/src/components/JobListings.vue
+++ frontend/src/components/JobListings.vue
@@ -1,6 +1,7 @@
 <script setup>
 import jobData from '@/mock/jobs.json';
 import { ref, defineProps } from 'vue';
+import { RouterLink } from 'vue-router';
 
 import JobListing from './JobListing.vue';
 
@@ -27,7 +28,8 @@ const jobs = ref(jobData);
     </div>
   </section>
   <section v-if="showButton" class="m-auto max-w-lg my-10 px-6">
-    <a href="/jobs" class="block bg-black text-white text-center py-4 px-6 rounded-xl hover:bg-gray-700">View All
-      Jobs</a>
+    <RouterLink to="/jobs" class="block bg-black text-white text-center py-4 px-6 rounded-xl hover:bg-gray-700">
+      View All Jobs
+    </RouterLink>
   </section>
 </template>
