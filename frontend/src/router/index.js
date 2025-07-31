77bb1f3f580a0946c0d035c5055ce546b3b6e80a
Ryan Paixao <16196485+ryanpaixao@users.noreply.github.com>
2025-07-31 20:47:47 -0300

    Create JobsView, NotFoundView. Update Router

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,5 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import HomeView from '@/views/HomeView.vue';
+import JobsView from '@/views/JobsView.vue';
+import NotFoundView from '@/views/NotFoundView.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -9,6 +11,16 @@ const router = createRouter({
       name: 'home',
       component: HomeView,
     },
+    {
+      path: '/jobs',
+      name: 'jobs',
+      component: JobsView,
+    },
+    {
+      path: '/:catchAll(.*)',
+      name: 'not-found',
+      component: NotFoundView
+    }
   ],
 });
 
