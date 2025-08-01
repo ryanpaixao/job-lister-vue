d4261ae055d9d63a5df2dd44379e9946334d58d2
Ryan Paixao <16196485+ryanpaixao@users.noreply.github.com>
2025-08-01 14:40:29 -0300

    Add dep: vue-spinner. Add spinner to JobListings

--- frontend/src/components/JobListings.vue
+++ frontend/src/components/JobListings.vue
@@ -1,7 +1,8 @@
 <script setup>
-import { ref, defineProps, onMounted } from 'vue';
+import { reactive, defineProps, onMounted } from 'vue';
 import { RouterLink } from 'vue-router';
 import axios from 'axios';
+import PulseLoader from 'vue-spinner/src/PulseLoader.vue';
 
 import JobListing from './JobListing.vue';
 
@@ -13,14 +14,19 @@ defineProps({
   },
 });
 
-const jobs = ref([]);
+const state = reactive({
+  jobs: [],
+  isLoading: true,
+});
 
 onMounted(async () => {
   try {
     const response = await axios.get('http://localhost:5000/jobs');
-    jobs.value = response.data;
+    state.jobs = response.data;
   } catch (error) {
     console.log('Error fetching jobs: ', error);
+  } finally {
+    state.isLoading = false;
   }
 });
 </script>
@@ -31,8 +37,11 @@ onMounted(async () => {
       <h2 class="text-3xl font-bold text-green-500 mb-6 text-center">
         Browse Jobs
       </h2>
-      <div class="grid grid-cols-1 md:grid-cols-3 gap-6">
-        <JobListing v-for="job in jobs.slice(0, limit || jobs.length)" :key="job.id" :job="job" />
+      <div v-if="state.isLoading" class="text-center text-gray-500 py-6">
+        <PulseLoader />
+      </div>
+      <div v-else class="grid grid-cols-1 md:grid-cols-3 gap-6">
+        <JobListing v-for="job in state.jobs.slice(0, limit || state.jobs.length)" :key="job.id" :job="job" />
       </div>
     </div>
   </section>
